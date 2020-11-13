db4fa1eb3d1282fdb6b19586085b20c7b39266b7
Boban Davidovic <boban.da87@gmail.com>
2020-11-13 20:17:30 +0100

    Adding Algolia rules for log in order to collect new data automatically and to index logs

--- app/Models/LogAggregator.php
+++ app/Models/LogAggregator.php
@@ -13,9 +13,64 @@ use Laravel\Scout\Searchable;
 class LogAggregator extends Model
 {
     use Searchable;
-    
+
+    public const VALIDATION_FIELDS = [
+        'log' => 'required|string',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Define fillable attributes
+     *
+     * @var array
+     */
     protected $fillable = [
         'log',
     ];
 
+    /**
+     * Get the index name for the model.
+     *
+     * @return string
+     */
+    public function searchableAs()
+    {
+        return 'logs_index';
+    }
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Get the value used to index the model.
+     *
+     * @return mixed
+     */
+    public function getScoutKey()
+    {
+        return $this->log;
+    }
+
+    /**
+     * Get the key name used to index the model.
+     *
+     * @return mixed
+     */
+    public function getScoutKeyName()
+    {
+        return 'log';
+    }
 }
