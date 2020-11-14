a0ec6affe03a0f44974587dbb6f0acf2c70b6fbc
Boban Davidovic <boban.da87@gmail.com>
2020-11-14 13:27:26 +0100

    Removing unused routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth:web')->get('/home', 'HomeController@index')->name('home');
 Route::middleware('auth:web')->get('/', 'HomeController@index');
-Route::middleware('auth:web')->get('logs', 'LogAggregatorController@index');
 
